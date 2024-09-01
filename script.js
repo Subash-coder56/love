8f69c270f296848ba644f66a944914b6663c5978
Subash-coder56 <subash15461645@gmail.com>
2024-08-31 22:03:01 -0700

    rttrttrt

---
+++ script.js
@@ -0,0 +1,14 @@
+const b = document.querySelector('button.btn-no')
+b.addEventListener("mouseover",moveHover);
+
+
+
+function moveHover(){
+    const i = Math.floor(Math.random()*500)+1;
+    const j = Math.floor(Math.random()*500)+1;
+
+
+    b.style.left = i+"px";
+    b.style.top = j+"px";
+
+}
